15bd36e543431372e071a079fae37a57c5be4c1e
Kevin Ligan <kevinligan04@gmail.com>
2023-09-13 10:16:01 +0800

    modified to change sample project

--- demos.py
+++ demos.py
@@ -1,8 +1,3 @@
-def display_numbers(x, y):
-    return 10
-
-x = 3
-y = 4
-x = display_numbers(x, y)
-
-print(x)
+color = input('Please type your favorite color: ')
+print ('Your favorite color is')
+print (color)
